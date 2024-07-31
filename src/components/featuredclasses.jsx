6bf50e0765c487f6ac3dba8271eabc1dece85fb5
was351 <wasif_farhan@hotmail.com>
2024-07-30 18:31:22 -0700

    dwns

--- src/components/featuredclasses.jsx
+++ src/components/featuredclasses.jsx
@@ -4,26 +4,30 @@ const FeaturedClasses = () => {
   return (
     <div className="featured">
       <div className="featuredItem">
-        <img
-          src="hero.png"
-          alt="Introductory Coding Class"
-          className="featuredImg"
-        />
-        <div className="featuredTitles">
-          <span className="fcName">Introductory Coding Class (Vancouver Area)</span>
-          <span className="fcGradeLevel"> Grades: 6th to 9th</span>
-        </div>
+        <a href="https://booking.codepulse.ca/classes/66a97fe7867585725aa46492">
+          <img
+            src="hero.png"
+            alt="Introductory Coding Class"
+            className="featuredImg"
+          />
+          <div className="featuredTitles">
+            <span className="fcName">Introductory Coding Class (Vancouver Area)</span>
+            <span className="fcGradeLevel"> Grades: 6th to 9th</span>
+          </div>
+        </a>
       </div>
       <div className="featuredItem">
-        <img
-          src="fundemental.png"
-          alt="Coding Fundamentals"
-          className="featuredImg"
-        />
-        <div className="featuredTitles">
-          <span className="fcName">Coding Fundamentals (Vancouver Area)</span>
-          <span className="fcGradeLevel"> Grades: 8th to 12th</span>
-        </div>
+        <a href="https://booking.codepulse.ca/classes/66a97fe7867585725aa46492">
+          <img
+            src="fundemental.png"
+            alt="Coding Fundamentals"
+            className="featuredImg"
+          />
+          <div className="featuredTitles">
+            <span className="fcName">Coding Fundamentals (Vancouver Area)</span>
+            <span className="fcGradeLevel"> Grades: 8th to 12th</span>
+          </div>
+        </a>
       </div>
     </div>
   );
